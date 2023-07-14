dce4db15152e68117e1545d3a561a2c2707283f7
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2023-07-14 19:20:21 +0700

    added nodemon and start storage

--- storage.js
+++ storage.js
@@ -1,5 +1,27 @@
+const fs = require("fs");
+const storage_path = process.env.STORAGE_PATH
+  ? process.env.STORAGE_PATH
+  : "./storage_folder";
+
 getList = () => {
-  return ["asdf.jpg", "asdf.png"];
+  if (!isExist(storage_path)) {
+    return [];
+  }
+  return fs.readdirSync(storage_path);
 };
 
-module.exports = { getList: getList };
+initMount = () => {
+  if (!isExist(storage_path)) {
+    fs.mkdirSync(storage_path);
+  }
+};
+
+isExist = (path) => {
+  if (fs.existsSync(path)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { getList: getList, initMount: initMount };
